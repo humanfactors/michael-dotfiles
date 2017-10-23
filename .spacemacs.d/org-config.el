df2e52456954ba410c4b5de355378111602bfa27
Michael David Wilson <michaelwilson1991@gmail.com>
2017-10-23 21:08:04 +0800

    org config

--- .spacemacs.d/org-config.el
+++ .spacemacs.d/org-config.el
@@ -1,8 +1,38 @@
 ;; Org Directory
 (global-set-key (kbd "C-x M-1") (lambda() (interactive)(find-file "~/Dropbox/org/")))
 
+;; Inline code evaluation
+(setq org-confirm-babel-evaluate nil)
+(require 'ob-python)
+(require 'ob-R)
+
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((python . t)
+    (R . t)))
+
 ;; Org Configuration
 (with-eval-after-load 'org
+  (setq org-agenda-files '("~/Dropbox/org/"))
+  (setq org-agenda-custom-commands
+        '(("c" "Simple agenda view"
+           ((agenda "")
+            (alltodo "")))))
+
+  (setq org-publish-project-alist
+      '(("org"
+        :base-directory "~/Dropbox/org/"
+        :publishing-directory "~/Dropbox/org/html"
+        :publishing-function org-html-publish-to-html
+        :section-numbers nil
+        :with-toc nil
+        :html-head "<link rel=\"stylesheet\"
+                    href=\"solarized-light.css\"
+                    type=\"text/css\"/>"
+        :auto-sitemap t                ; Generate sitemap.org automagically...
+        :sitemap-filename "index.org"
+        :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+        )))
 
   (org-defkey org-mode-map [(meta return)] 'org-meta-return)
 
