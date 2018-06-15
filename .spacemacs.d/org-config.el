c8198301753c1f8bc75a6c543ccd6a5e07f8cd7f
Michael Wilson <michael@humanfactors.io>
2018-06-15 17:43:06 +0800

    Indentation and new capture templates

--- .spacemacs.d/org-config.el
+++ .spacemacs.d/org-config.el
@@ -5,36 +5,37 @@
 (with-eval-after-load 'org
   ;; (setq org-agenda-files '("~/Dropbox/org/"))
   ;; (setq org-agenda-custom-commands
-        ;; '(("c" "Simple agenda view"
-           ;; ((agenda "")
-            ;; (alltodo "")))))
+  ;; '(("c" "Simple agenda view"
+  ;; ((agenda "")
+  ;; (alltodo "")))))
 
 
   ;; Inline code evaluation
   (setq org-confirm-babel-evaluate nil)
   ;; (org-babel-do-load-languages
-   ;; 'org-babel-load-languages
-   ;; '((R . t)))
+  ;; 'org-babel-load-languages
+  ;; '((R . t)))
 
+  (setq org-directory "~/Dropbox/org/")
 
   (setq org-agenda-files (list "~/Dropbox/org/"
                                "~/Dropbox/Thesis"
                                ))
 
   (setq org-publish-project-alist
-      '(("org"
-        :base-directory "~/Dropbox/org/"
-        :publishing-directory "~/Dropbox/org/html"
-        :publishing-function org-html-publish-to-html
-        :section-numbers nil
-        :with-toc nil
-        :html-head "<link rel=\"stylesheet\"
+        '(("org"
+           :base-directory "~/Dropbox/org/"
+           :publishing-directory "~/Dropbox/org/html"
+           :publishing-function org-html-publish-to-html
+           :section-numbers nil
+           :with-toc nil
+           :html-head "<link rel=\"stylesheet\"
                     href=\"solarized-light.css\"
                     type=\"text/css\"/>"
-        :auto-sitemap t                ; Generate sitemap.org automagically...
-        :sitemap-filename "index.org"
-        :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
-        )))
+           :auto-sitemap t                ; Generate sitemap.org automagically...
+           :sitemap-filename "index.org"
+           :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+           )))
 
   (org-defkey org-mode-map [(meta return)] 'org-meta-return)
 
@@ -47,8 +48,8 @@
   (defun org-insert-subheading-end ()
     (interactive)
     (when (org-at-heading-p)
-    (end-of-line)
-    (call-interactively 'org-insert-subheading )))
+      (end-of-line)
+      (call-interactively 'org-insert-subheading )))
 
   (add-hook 'org-mode-hook (lambda ()
                              (spacemacs/set-leader-keys "obr" 'org-tree-open-in-right-frame)
@@ -64,8 +65,21 @@
   (setq org-capture-templates
         '(("t" "Todo" entry (file+headline "~/Dropbox/org/TODOs.org" "Tasks")
            "* TODO %?\n  %i\n  %a")
+
           ("j" "Journal" entry (file+datetree "~/Dropbox/org/diary.org")
-           "* %?\nEntered on %U\n  %i\n")))
+           "* %?\nEntered on %U\n  %i\n")
+
+          ("c" "Code Snippet" entry (file "~/Dropbox/org/code.org")
+           ;; Prompt for tag and language
+           "** %^{Snippet description}\t%^g\n#+BEGIN_SRC %^{language}\n%?\n#+END_SRC\n")
+
+          ("d" "Thesis Discussion Idea" entry (file+headline "~/Dropbox/Thesis/thesis.org" "Discussion Ideas")
+           "** %^{Idea Title}\n %?%i \n")
+
+          ("i" "Thesis Introduction Idea" entry (file+headline "~/Dropbox/Thesis/thesis.org" "Introduction Ideas")
+           "** %^{Idea Title}\n %?%i \n")
+
+          ))
   (setq org-todo-keywords
-        '((sequence "TODO" "REMEMBER" "|" "DONE" "CANCELLED")))
+        '((sequence "TODO" "REMEMBER" "WAITING" "|" "DONE" "CANCELLED")))
   )
