8c620016ee7e2b431e80e96f08a06a05fb9dcbd3
Michael David Wilson <michaelwilson1991@gmail.com>
2018-07-18 23:15:09 +0800

    actually that was it

--- .spacemacs.d/modes-config.el
+++ .spacemacs.d/modes-config.el
@@ -56,10 +56,13 @@
 (add-to-list 'auto-mode-alist '("\\.rmd\\'" . markdown-mode))
 
 ;; Fix inline codeblocks being split in markdown mode in Rmarkdown documents when filling
-;; (add-hook 'fill-nobreak-predicate
-;; #'markdown-inline-code-at-point-p)
+(with-eval-after-load 'markdown-mode
+  (add-hook 'fill-nobreak-predicate
+            #'markdown-inline-code-at-point-p))
 
-
+(with-eval-after-load 'projectile
+  (setq projectile-switch-project #'projectile-find-dir)
+  (setq projectile-find-dir-includes-top-level t))
 ;; Default to insert git commit
 (add-hook 'git-commit-mode-hook 'evil-insert-state)
 
@@ -67,11 +70,43 @@
 ;; No more _ to <-
 (setq ess-disable-underscore-assign t)
 
-;; no more fancy comments
+(with eval-after-load 'easy-hugo
 
-(add-hook 'ess-mode-hook
-          (lambda()
-            (setq ess-indent-level 2
-                  tab-width 2)
-            (setq ess-fancy-comments nil)
-            (setq ess-indent-with-fancy-comments nil)))
+;; Easy Hugo
+(defun cesco/easy-hugo ()
+  (interactive)
+  (evil-define-key
+    (list 'normal 'insert 'visual 'motion)
+    easy-hugo-mode-map
+    "n" 'easy-hugo-newpost
+    "D" 'easy-hugo-article
+    "p" 'easy-hugo-preview
+    "P" 'easy-hugo-publish
+    "o" 'easy-hugo-open
+    "d" 'easy-hugo-delete
+    "e" 'easy-hugo-open
+    "c" 'easy-hugo-open-config
+    "f" 'easy-hugo-open
+    "N" 'easy-hugo-no-help
+    "v" 'easy-hugo-view
+    "r" 'easy-hugo-refresh
+    "g" 'easy-hugo-refresh
+    "s" 'easy-hugo-sort-time
+    "S" 'easy-hugo-sort-char
+    "G" 'easy-hugo-github-deploy
+    "A" 'easy-hugo-amazon-s3-deploy
+    "C" 'easy-hugo-google-cloud-storage-deploy
+    "q" 'evil-delete-buffer
+    (kbd "TAB") 'easy-hugo-open
+    (kbd "RET") 'easy-hugo-preview))
+
+(define-key global-map (kbd "C-c <f9>") 'easy-hugo)
+(add-hook 'easy-hugo-mode-hook 'cesco/easy-hugo)
+)
+
+(setq doc-view-continuous t)
+
+
+(with-eval-after-load 'deft
+  (define-key deft-mode-map (kbd "C-<return>") 'deft-new-file)
+  )
