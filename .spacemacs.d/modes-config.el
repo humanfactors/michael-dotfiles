4322602dbcd54e6c8d66aa8273927a7d07fb2ed1
Michael David Wilson <michaelwilson1991@gmail.com>
2018-10-01 18:31:51 +0800

    Multicurosrs

--- .spacemacs.d/modes-config.el
+++ .spacemacs.d/modes-config.el
@@ -5,6 +5,12 @@
 (setq tab-width 2)
 
 ;; Go mode
+;; Mulitple Cursors
+
+(require 'multiple-cursors)
+(global-set-key (kbd "<f5> SPC") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
 ;; Shell Config
 ;; (setq explicit-shell-file-name "/bin/bash")
