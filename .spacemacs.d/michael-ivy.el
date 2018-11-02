4a7bd9dff5d9b648d9bb5753d181a0e71277b039
humanfactors <michaelwilson1991@gmail.com>
2018-11-02 11:05:09 +0800

    Ivy yank fix

---
+++ .spacemacs.d/michael-ivy.el
@@ -0,0 +1,5 @@
+;; http://pragmaticemacs.com/emacs/counsel-yank-pop-with-a-tweak/
+;;  I use the following code to configure counsel-yank-pop to replace the standard yank-pop on M-y. The only thing I missed about the vanilla yank-pop was that repeatedly pressing M-y cycled through the entries. The counsel version doesn’t do this by default but this is easy to add by binding M-y to ivy-next-line in the ivy-minibuffer-map. 
+(with-eval-after-load 'counsel
+  (global-set-key  "\M-y" 'yank-pop)
+  (define-key ivy-minibuffer-map (kbd "M-y") 'ivy-next-line))
--- .spacemacs.d/windows.el
+++ .spacemacs.d/windows.el
@@ -1,7 +1,5 @@
-(setq w32-pass-lwindow-to-system nil)
-(setq w32-lwindow-modifier 'super) ; Left Windows key
-
-(global-set-key [M-f4] 'spacemacs/frame-killer)
-(global-set-key (kbd "C-'") 'w32-browser)
-
+;; (require 'w32-browser)
+;; (setq w32-pass-lwindow-to-system nil)
+;; (setq w32-lwindow-modifier 'super) ; Left Windows key
+(global-set-key [M-f4] 'spacemacs/frame-killer) 
 
