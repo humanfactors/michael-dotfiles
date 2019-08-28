ff7c2ecc53353964d31e79244cc9d282ab6a9e3d
humanfactors <michaelwilson1991@gmail.com>
2019-08-28 09:56:52 +0800

    Added R custom funcs

---
+++ R/myFuncs.R
@@ -0,0 +1,73 @@
+# https://stackoverflow.com/questions/25576358/calculate-cumsum-while-ignoring-na-values
+cumSkipNA <- function(x, FUNC)
+{
+  d <- deparse(substitute(FUNC))
+  funs <- c("max", "min", "prod", "sum")
+  stopifnot(is.vector(x), is.numeric(x), d %in% funs)
+  FUNC <- match.fun(paste0("cum", d))
+  x[!is.na(x)] <- FUNC(x[!is.na(x)])
+  x
+}
+
+# Load my custom favourite packages
+pkgLoad <- function( packages = "favourites" ) {
+
+    if( length( packages ) == 1L && packages == "favourites" ) {
+        packages <- c( "tidyverse", "ggplot2", "dplyr", "testthat"
+        )
+    }
+
+    packagecheck <- match( packages, utils::installed.packages()[,1] )
+
+    packagestoinstall <- packages[ is.na( packagecheck ) ]
+
+    if( length( packagestoinstall ) > 0L ) {
+        utils::install.packages( packagestoinstall,
+                             repos = "http://cran.csiro.au"
+        )
+    } else {
+        print( "All requested packages already installed" )
+    }
+
+    for( package in packages ) {
+        suppressPackageStartupMessages(
+            library( package, character.only = TRUE, quietly = TRUE )
+        )
+    }
+
+}
+
+# Load the required data
+load_sqlite3_data <- function(databasepath, tablename) {
+  sqlite.driver <- dbDriver("SQLite")
+  db <- dbConnect(sqlite.driver, dbname = databasepath)
+  table <- dbReadTable(db,tablename)
+  dbDisconnect(db)
+  return(as_tibble(table))
+}
+
+remove_extra_quotes <- function(.data, name){
+  name <- enquo(name)
+  .data %>% 
+    mutate(!!quo_name(name) := gsub("\"","", !!name))
+}
+
+not_all_missing <- function(x) any(!is.na(x))
+
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+'%ni%' <- Negate('%in%')
+
+ggplotRegression <- function (fit) {
+
+  require(ggplot2)
+
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
